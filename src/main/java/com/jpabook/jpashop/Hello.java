45519bbd75a251936fb4ed82d50ce925399ddd00
hunputer <wlgns3855@gmail.com>
2023-03-22 22:47:53 +0900

    commit

---
+++ src/main/java/com/jpabook/jpashop/Hello.java
@@ -0,0 +1,10 @@
+package com.jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+}
---
+++ src/main/java/com/jpabook/jpashop/Member.java
@@ -0,0 +1,18 @@
+package com.jpabook.jpashop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+}
